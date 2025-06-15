acd85b4756bbd169affc9d3ce4b31c4df665ac56
Caleb <calebnull88@outlook.com>
2025-06-15 11:55:50 -0500

    change some ui

--- src/app/dashboard/create/page.tsx
+++ src/app/dashboard/create/page.tsx
@@ -1,7 +1,16 @@
-"use client";
 import SignOutAction from "@/components/actions/SignOutAction";
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
 
-export default function SignOut() {
+export default async function SignOut() {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  if (session) {
+    console.log(session);
+  }
+
   return (
     <div className="min-h-screen w-full flex p-[25px] justify-between items-center">
       <h1>Create</h1>
