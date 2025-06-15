bee3483a012b58ccb18ebdefd1ba27318b66a575
Caleb <calebnull88@outlook.com>
2025-06-15 11:58:37 -0500

    user info added to DB

--- server/users.ts
+++ server/users.ts
@@ -1,26 +1,22 @@
 "use server";
 
 import { auth } from "../src/lib/auth";
-import { createHash } from "@better-auth/utils/hash";
 
-export const signIn = async () => {
-  const hashBuffer = await createHash("SHA-256").digest("text");
-  const hashInHex = await createHash("SHA-256", "hex").digest("text");
-  
+export const signIn = async (email: string, password: string) => {
   const response = await auth.api.signInEmail({
     body: {
-      email: "soto@test.com",
-      password: "password1234",
+      email,
+      password,
     },
   })
 }
 
-export const signUp = async () => {
+export const signUp = async (email: string, password: string, name: string) => {
   await auth.api.signUpEmail({
     body: {
-      email: "soto@test.com",
-      password: "password1234",
-      name: "Soto",
+      email,
+      password,
+      name,
     },
-  })
-}
+  });
+};
